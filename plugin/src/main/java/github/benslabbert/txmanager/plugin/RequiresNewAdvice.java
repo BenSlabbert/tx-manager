891520deb496767acce315824b4aefea4bae5f70
benjamin.slabbert <benjamin.slabbert@gmail.com>
2024-12-30 13:14:21 +0100

    updating

--- plugin/src/main/java/github/benslabbert/txmanager/plugin/RequiresNewAdvice.java
+++ plugin/src/main/java/github/benslabbert/txmanager/plugin/RequiresNewAdvice.java
@@ -4,31 +4,40 @@ package github.benslabbert.txmanager.plugin;
 import github.benslabbert.txmanager.PlatformTransactionManager;
 import java.util.Arrays;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.AllArguments;
+import net.bytebuddy.asm.Advice.FieldValue;
+import net.bytebuddy.asm.Advice.OnMethodEnter;
+import net.bytebuddy.asm.Advice.OnMethodExit;
+import net.bytebuddy.asm.Advice.Origin;
+import net.bytebuddy.asm.Advice.Thrown;
 import org.slf4j.Logger;
 
-public class RequiresNewAdvice {
+class RequiresNewAdvice {
 
   private RequiresNewAdvice() {}
 
-  @Advice.OnMethodEnter
+  @OnMethodEnter
   private static void onEnter(
-      @Advice.AllArguments(nullIfEmpty = true) Object[] args,
-      @Advice.Origin("#m") String methodName,
-      @Advice.FieldValue(value = "log") Logger log) {
+      @AllArguments(nullIfEmpty = true) Object[] args,
+      @Origin("#m") String methodName,
+      @FieldValue(value = "log") Logger log) {
 
-    log.debug("Entering advised method: {}", methodName);
-    log.debug("args: {}", args == null ? "null" : Arrays.toString(args));
+    if (log.isDebugEnabled()) {
+      log.debug("Entering advised method: {}", methodName);
+      log.debug("args: {}", args == null ? "null" : Arrays.toString(args));
+    }
 
+    log.debug("begin transaction method: {}", methodName);
     PlatformTransactionManager.begin();
   }
 
-  @Advice.OnMethodExit(onThrowable = Exception.class)
+  @OnMethodExit(onThrowable = Exception.class)
   private static void onExit(
-      @Advice.Origin("#m") String methodName,
-      @Advice.Thrown Throwable throwable,
-      @Advice.FieldValue(value = "log") Logger log) {
+      @Origin("#m") String methodName,
+      @Thrown Throwable throwable,
+      @FieldValue(value = "log") Logger log) {
 
-    log.info("Exiting advised method: {}", methodName);
+    log.debug("Exiting advised method: {}", methodName);
 
     if (throwable != null) {
       log.error("Exception thrown in advised method: {}", methodName, throwable);
@@ -36,6 +45,7 @@ public class RequiresNewAdvice {
       return;
     }
 
+    log.debug("commit after method: {}", methodName);
     PlatformTransactionManager.commit();
   }
 }
