c5e3795232871bd04fcc6836ae166db7c959f090
Ben <benjamin.slabbert@gmail.com>
2024-12-31 19:33:15 +0000

    fmt

--- plugin/src/main/java/github/benslabbert/txmanager/plugin/RequiresNewAdvice.java
+++ plugin/src/main/java/github/benslabbert/txmanager/plugin/RequiresNewAdvice.java
@@ -1,19 +1,20 @@
+/* Licensed under Apache-2.0 2024. */
 package github.benslabbert.txmanager.plugin;
 
-import net.bytebuddy.asm.Advice;
 import github.benslabbert.txmanager.annotation.Transactional;
+import net.bytebuddy.asm.Advice;
 
 public class RequiresNewAdvice {
 
-    @Advice.OnMethodEnter
-    public static void onEnter(@Transactional Transactional transactional) {
-        // Use the properties here
-        System.out.println("Propagation: " + transactional.propagation());
-        System.out.println("doNotRollBackFor: " + Arrays.toString(transactional.doNotRollBackFor()));
-    }
+  @Advice.OnMethodEnter
+  public static void onEnter(@Transactional Transactional transactional) {
+    // Use the properties here
+    System.out.println("Propagation: " + transactional.propagation());
+    System.out.println("doNotRollBackFor: " + Arrays.toString(transactional.doNotRollBackFor()));
+  }
 
-    @Advice.OnMethodExit
-    public static void onExit() {
-        // Implement any necessary logic here
-    }
+  @Advice.OnMethodExit
+  public static void onExit() {
+    // Implement any necessary logic here
+  }
 }
