804d4f4e578a5b122390cf87e233c59b059c7ace
Ben <benjamin.slabbert@gmail.com>
2024-12-31 19:45:29 +0000

    some fixes, but might be on the wrongway

--- annotation/src/main/java/github/benslabbert/txmanager/annotation/Transactional.java
+++ annotation/src/main/java/github/benslabbert/txmanager/annotation/Transactional.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(value = {ElementType.METHOD, ElementType.TYPE})
+@Target(value = {ElementType.METHOD, ElementType.TYPE, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Transactional {
 
