d05686cc7786b0ec76438ab85ec9bfcbf42f7eee
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-02-02 17:42:40 +0100

    adding bom

--- txmanager/src/main/java/github/benslabbert/txmanager/TransactionManager.java
+++ txmanager/src/main/java/github/benslabbert/txmanager/TransactionManager.java
@@ -1,13 +1,34 @@
 /* Licensed under Apache-2.0 2024. */
 package github.benslabbert.txmanager;
 
+import java.sql.Connection;
+
 public interface TransactionManager extends AutoCloseable {
+
+  Connection getConnection();
 
   void begin();
 
   void ensureActive();
 
   void commit();
+
+  /**
+   * Execute the {@link Runnable} before a transaction is committed.<br>
+   * This can ben called multiple times with different {@link Runnable}(s) which will be executed in
+   * no specified order.<br>
+   * The existing underlying transaction will be available.
+   */
+  void beforeCommit(Runnable runnable);
+
+  /**
+   * Execute the {@link Runnable} after a transaction is committed.<br>
+   * This can ben called multiple times with different {@link Runnable}(s).<br>
+   * The previous transaction will not be available.<br>
+   * Provided {@link Runnable}(s) will be scheduled only after a successful commit and can be run in
+   * parallel.
+   */
+  void afterCommit(Runnable runnable);
 
   void rollback();
 }
